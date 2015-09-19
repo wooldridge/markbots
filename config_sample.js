6fb89a9b1a9a9f909bc3f72750d11399c5882748
Mike Wooldridge <mike@hellion.org>
2015-09-19 16:45:16 -0700

    add sample config file, ignore config.js

---
+++ config_sample.js
@@ -0,0 +1,24 @@
+var config = {};
+
+config.bot = {
+  id: 'BOTID' // e.g., 'markbot1'
+}
+
+// https://www.raspberrypi.org/documentation/raspbian/applications/camera.md
+config.raspicam = {
+  mode: "photo",
+  preview: '0,0,640,480',
+  encoding: "jpg",
+  timeout: 2000, // millisecs
+  verbose: true
+}
+
+config.marklogic = {
+  host: 'SERVER.EXAMPLE.COM',
+  port:	'8000',
+  user:	'USER',
+  password: 'PASSWORD',
+  authType: 'digest'
+}
+
+module.exports = config;
