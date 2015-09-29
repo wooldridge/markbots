a6873edd119e38e559b359a3285d84e2e0830655
Mike Wooldridge <mike@hellion.org>
2015-09-29 02:45:35 -0400

    fix selector

--- dashboard/js/dash.js
+++ dashboard/js/dash.js
@@ -101,8 +101,7 @@ $(document).ready(function () {
                 if (data.length > 0) {
                   // for now, assume will be one (since we have two bots)
                   var dist = data[0].dist * 1000; // meters
-                  $('tr#row_' + b.getId() + ' td#marklogic1!
-                    col_nearby').html(dist.toFixed(2) + ' m');
+                  $('tr#row_' + b.getId() + ' td#col_nearby').html(dist.toFixed(2) + ' m');
                 }
             });
           }
