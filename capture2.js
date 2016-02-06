95dc8c89a2bca92e829836d40cbce473dbdfe9e0
Mike Wooldridge <mike@hellion.org>
2016-02-06 11:49:14 -0800

    Use ISO 8601 for timestamp format

--- capture2.js
+++ capture2.js
@@ -118,13 +118,14 @@ socket.on('multi', function(data){
 
 function getTimestamp () {
   var d = new Date();
-  var ts =
-    d.getFullYear() +'-'+
-    ('0' + (d.getMonth()+1)).slice(-2) +'-'+
-    ('0' + d.getDate()).slice(-2) + '_' +
-    ('0' + d.getHours()).slice(-2) + '-' +
-    ('0' + d.getMinutes()).slice(-2) + '-' +
-    ('0' + d.getSeconds()).slice(-2);
+  // var ts =
+  //   d.getFullYear() +'-'+
+  //   ('0' + (d.getMonth()+1)).slice(-2) +'-'+
+  //   ('0' + d.getDate()).slice(-2) + '_' +
+  //   ('0' + d.getHours()).slice(-2) + '-' +
+  //   ('0' + d.getMinutes()).slice(-2) + '-' +
+  //   ('0' + d.getSeconds()).slice(-2);
+  var ts = d.toISOString();
   return ts;
 }
 
